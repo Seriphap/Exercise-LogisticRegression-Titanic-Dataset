ac8b4befa0a271affe15a36094c2c7018131e97f
Seriphap Siangnok (Way) <siangnok@gmail.com>
2025-05-07 23:34:56 +0700

    Update Rawdata.py

--- pages/Rawdata.py
+++ pages/Rawdata.py
@@ -3,7 +3,3 @@ import pandas as pd
 
 st.set_page_config(page_title="Raw Data", page_icon="📄")
 
-file_id = "1w1XQ8RzDDOLrUpBoE9oqRSPL06QILEog"
-url = f"https://drive.google.com/uc?id={file_id}"
-df = pd.read_csv(url)
-st.dataframe(df)
