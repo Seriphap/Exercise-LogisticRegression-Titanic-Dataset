f2c46d7a90db83ef827c7576408e3059885231eb
Seriphap Siangnok (Way) <siangnok@gmail.com>
2025-05-07 23:14:37 +0700

    Update Rawdata.py

--- pages/Rawdata.py
+++ pages/Rawdata.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title="Raw Data", 
-                   page_icon="📄")
+st.set_page_config(page_title="Raw Data", page_icon="📄")
 
 df = pd.read_csv('/content/drive/MyDrive/Colab Notebooks/MyData/titanic_train.csv')
-st.display(df)
+st.dataframe(df)
